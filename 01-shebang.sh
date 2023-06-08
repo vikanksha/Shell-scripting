fa62c51ed2d232696b91b512df0f5ac5dcc3f6ac
unknown <vikankshakumari09@gmail.com>
2023-06-08 15:54:00 +0530

    updated

--- 01-shebang.sh
+++ 01-shebang.sh
@@ -10,3 +10,9 @@ echo -e "\e[43;32m Hello this is my first script in bash color coding using gree
 echo -e "\e[40;33m hello this is yellow color \e[0m"
 echo -e "\e[45;34m hello this is blue color \e[0m"
 echo -e "\e[42;36m hello this is cyan color \e["0m
+a=10
+name=Vikanksha
+batch=54
+topic=shellscripting
+echo $a
+echo value of the variables a is $a

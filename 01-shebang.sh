547d2837b5ae2a9dda0747c742dc5f40f67c9f95
unknown <vikankshakumari09@gmail.com>
2023-06-08 16:44:20 +0530

    updated

--- 01-shebang.sh
+++ 01-shebang.sh
@@ -10,9 +10,12 @@
 #echo -e "\e[40;33m hello this is yellow color \e[0m"
 #echo -e "\e[45;34m hello this is blue color \e[0m"
 #echo -e "\e[42;36m hello this is cyan color \e["0m
-a=10
-name=Vikanksha
-batch=54
-topic=shellscripting
-echo $a
-echo -e "value of the variables a is \e[32m $a\e[0m"
+# a=10
+#name=Vikanksha
+#batch=54
+#topic=shellscripting
+#echo $a
+# echo -e "value of the variables a is \e[32m $a\e[0m"
+# TODAYS_DATE = "08-06-2023"
+TODAYS_DATE = $(date +%D)
+echo -e "Good morning and todays date is\e[34m $TODAYS_DATE\3[0m"

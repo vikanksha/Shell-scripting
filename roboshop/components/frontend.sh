1fe4257aabae39668a9a32282161c8120e54ddf7
unknown <vikankshakumari09@gmail.com>
2023-06-10 20:32:08 +0530

    update

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 
-echo "I am cart"
+echo "Installing nginx"
+
+yum install nginx -y
+
+# Let's download the HTDOCS content and deploy it under the Nginx path.
+
+# ```
+# curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+
+# ```
+
+# Deploy in Nginx Default Location.
+
+# ```
+# cd /usr/share/nginx/html
+# rm -rf *
+# unzip /tmp/frontend.zip
+# mv frontend-main/* .
+# mv static/* .
+# rm -rf frontend-main README.md
+# mv localhost.conf /etc/nginx/default.d/roboshop.conf
+
+# ```
+
+# - Finally, restart the service once to effect the changes.
+# - Now, you should be able to access the ROBOSHOP e-commerce webpage as shown below

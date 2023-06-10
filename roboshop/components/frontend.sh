33748796027be0a635b255a72d60ebcda7def396
unknown <vikankshakumari09@gmail.com>
2023-06-10 20:45:27 +0530

    check

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -2,7 +2,7 @@
 
 ID=$(id -u)
 if  [$ID -ne 0] ; then
-    echo -m "This script is expected to run by a root user or with sudo previledge"
+    echo -e "\e[33m This script is expected to run by a root user or with sudo previledge\e[0m"
     exit 1
 fi
 

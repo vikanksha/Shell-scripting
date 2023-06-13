8bf5df39efdd74e0e49013a563f13845112e3249
unknown <vikankshakumari09@gmail.com>
2023-06-13 12:31:03 +0530

    update

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -28,17 +28,17 @@ else
      echo -e "\e[31m failure \e[0m"
 fi
 
-# Let's download the HTDOCS content and deploy it under the Nginx path.
+echo -n "performing cleanup: "
+cd /usr/share/nginx/html
+rm -rf * &>> "/tmp/${COMPONENT}.log"
 
-# ```
-# curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+if [ $? -eq 0 ] ; then
+     echo -e "\e[32m success \e[0m"
+else
+     echo -e "\e[31m failure \e[0m"
+fi
 
-# ```
 
-# Deploy in Nginx Default Location.
-
-# ```
-# cd /usr/share/nginx/html
 # rm -rf *
 # unzip /tmp/frontend.zip
 # mv frontend-main/* .

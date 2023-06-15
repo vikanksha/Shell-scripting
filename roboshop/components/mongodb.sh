288350a426a83aaa288491331296b8189c7bef0d
unknown <vikankshakumari09@gmail.com>
2023-06-15 18:34:57 +0530

    autopush

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -25,9 +25,13 @@ curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans
 stat $?
 
 echo -n "Installing $COMPONENT : "
-  yum install -y mongodb-org &>> LOGFILE
+  yum install -y $COMPONENT-org &>> LOGFILE
   stat $?
-  
+ echo -n "Starting $COMPONENT : "
+ systemctl enable mongod &>> LOGFILE
+ systemctl start mongod &>> LOGFILE
+
+ 
 
 # Install Mongo & Start Service.
 

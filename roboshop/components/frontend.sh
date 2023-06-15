931e568aa611d168c80a17ff82b22d27515657d7
unknown <vikankshakumari09@gmail.com>
2023-06-15 17:31:46 +0530

    update

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -9,34 +9,31 @@ if  [$ID -ne 0] ; then
     exit 1
 fi
 
+stat() {
+
+    if [ $? -eq 0 ] ; then
+        echo -e "\e[32m success \e[0m"
+    else 
+        echo -e "\e[31m failure \e[0m"
+        exit 2
+    fi  
+}
+
 echo "Installing Nginx :"
 yum install nginx -y  &>> "/tmp/${COMPONENT}.log"
 
-if [ $? -eq 0 ] ; then
-    echo -e "\e[32m success \e[0m"
-else 
-    echo -e "\e[31m failure \e[0m"
-
-fi    
+stat $? 
 
 echo -n "Downloading the frontend component :"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
-if [ $? -eq 0 ] ; then
-     echo -e "\e[32m success \e[0m"
-else
-     echo -e "\e[31m failure \e[0m"
-fi
+stat $?
 
 echo -n "performing cleanup: "
 cd /usr/share/nginx/html
 rm -rf * &>> "/tmp/${COMPONENT}.log"
 
-if [ $? -eq 0 ] ; then
-     echo -e "\e[32m success \e[0m"
-else
-     echo -e "\e[31m failure \e[0m"
-fi
+stat $?
 
 
 # rm -rf *

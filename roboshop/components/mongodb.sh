caee6dcf03222ceef6b4e0add2f48d534e4a2e2e
unknown <vikankshakumari09@gmail.com>
2023-06-15 18:18:22 +0530

    new update

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,3 +1,48 @@
 #!/bin/bash
 
-echo "I am cart"
+COMPONENT=mongodb
+LOGFILE="/tmp/${COMPONENT}.log"
+
+ID=$(id -u)
+
+if  [$ID -ne 0] ; then
+    echo -e "\e[33m This script is expected to run by a root user or with sudo previledge \e[0m"
+    exit 1
+fi
+
+stat() {
+
+    if [ $? -eq 0 ] ; then
+        echo -e "\e[32m success \e[0m"
+    else 
+        echo -e "\e[31m failure \e[0m"
+        exit 2
+    fi  
+}
+
+echo -n "Configuring the $COMPONENT repo :"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
+stat $?
+
+# Install Mongo & Start Service.
+
+# yum install -y mongodb-org
+# systemctl enable mongod
+# systemctl start mongod
+# 1. Update Listen IP address from 127.0.0.1 to 0.0.0.0 in the config file, so that MongoDB can be accessed by other services.
+
+  # Config file:   # vim /etc/mongod.conf
+
+# - Then restart the service
+
+# ```bash
+# systemctl restart mongod
+#```
+
+  
+
+#- Every Database needs the schema to be loaded for the application to work.
+
+#---
+
+      #`Download the schema and inject it.`

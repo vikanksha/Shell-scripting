68f63ff638dee0ed1c1eb87f29ec203158a0f625
unknown <vikankshakumari09@gmail.com>
2023-06-22 07:23:59 +0530

    update

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -62,7 +62,7 @@ stat $?
 
 echo -n "Updating the $COMPONENT systemd file :"
 sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal' /home/${APPUSER}/${COMPONENT}/systemd.service
-mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
+mv /home/${APPUSER}/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
 stat $?
 
 echo -n "Starting ${COMPONENT} service :"

ba045965f386a7d9a9c469b721494abe23fd7112
unknown <vikankshakumari09@gmail.com>
2023-06-24 17:17:27 +0530

    checking

--- roboshop/components/user.sh
+++ roboshop/components/user.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
+COMPONENT=user
+LOGFILE="/tmp/${COMPONENT}.log"
 
-echo "I am cart"
+ID=$(id -u)
+
+if [ $ID -ne 0 ] ; then
+    echo -e "\e[33m This script is expected to run by a root user or with sudo previledge \e[0m"
+    exit 1
+fi
+
+stat() {
+
+    if [ $? -eq 0 ] ; then
+        echo -e "\e[32m success \e[0m"
+    else 
+        echo -e "\e[31m failure \e[0m"
+        exit 2
+    fi  
+}
+
+echo -e "*********** \e[35m $COMPONENT Installation has started \e[0m ***********"
+
+echo -e "Configuring the $COMPONENT repo :"
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -  &>> $LOGFILE
+stat $?
+
+echo -e "Installing NodeJS :"
+ yum install nodejs -y   &>> LOGFILE
+ stat $?

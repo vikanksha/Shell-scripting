73b95ac022a3f9e5ef4b9865df69b934639f0a34
unknown <vikankshakumari09@gmail.com>
2023-06-24 06:38:23 +0530

    update

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -60,18 +60,18 @@ cd /home/${APPUSER}/${COMPONENT}/
 npm install &>> $LOGFILE
 stat $?
 
-# echo -n "Updating the $COMPONENT systemd file :"
-# sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal' /home/${APPUSER}/${COMPONENT}/systemd.service
-# mv /home/${APPUSER}/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
-# stat $?
+echo -n "Updating the $COMPONENT systemd file :"
+sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal' /home/${APPUSER}/${COMPONENT}/systemd.service
+mv /home/${APPUSER}/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
+stat $?
 
-# echo -n "Starting ${COMPONENT} service :"
-# systemctl daemon-reload  &>> $LOGFILE
-# systemctl enable $COMPONENT  &>> $LOGFILE
-# systemctl restart $COMPONENT  &>> $LOGFILE
-# stat $?
+echo -n "Starting ${COMPONENT} service :"
+systemctl daemon-reload  &>> $LOGFILE
+systemctl enable $COMPONENT  &>> $LOGFILE
+systemctl restart $COMPONENT  &>> $LOGFILE
+stat $?
 
-# echo -e "************ \e[35m $COMPONENT Installation is completed \e[0m ************"
+echo -e "************ \e[35m $COMPONENT Installation is completed \e[0m ************"
 
 # mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
 # systemctl daemon-reload

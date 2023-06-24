9298549910292f7a9f286eaddaf48e4940c5217c
unknown <vikankshakumari09@gmail.com>
2023-06-24 16:56:57 +0530

    update

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -21,10 +21,25 @@ stat() {
 echo -e "*********** \e[35m $COMPONENT Installation has started \e[0m ***********"
 
 echo -n "Configuering the $COMPONENT repo :"
-curl -L https://raw.githubusercontent.com/stans-robot-project/${COMPONENT}/main/${COMPONENT}.repo -o /etc/yum.repos.d/${COMPONENT}.repo
+curl -L https://raw.githubusercontent.com/stans-robot-project/${COMPONENT}/main/${COMPONENT}.repo -o /etc/yum.repos.d/${COMPONENT}.repo 
 stat $?
 
 echo -n "Installing $COMPONENT :"
 yum install ${COMPONENT}-6.2.11 -y    &>> $LOGFILE
 stat $?
 
+echo -n "Enabling the DB visibility :"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/${COMPONENT}.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/${COMPONENT}/${COMPONENT}.conf
+stat $?
+
+echo -n "Starting $COMPONENT :"
+systemctl daemon-reload $COMPONENT  &>> $LOGFILE
+systemctl start $COMPONENT   &>> $LOGFILE
+sytemctl status $COMPONENT   &>> $LOGFILE
+stat $?
+
+
+
+# vim /etc/redis.conf
+# vim /etc/redis/redis.conf

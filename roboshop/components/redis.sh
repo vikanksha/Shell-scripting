6488f0157a4538bfac90b8582106e78cb3b4d14f
unknown <vikankshakumari09@gmail.com>
2023-06-24 16:41:09 +0530

    Update

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
+COMPONENT=redis
+LOGFILE="/tmp/${COMPONENT}.log"
 
-echo "I am cart"
+ID=$(id -u)
+
+if [ $ID -ne 0 ] ; then
+    echo -e "\e[33m This script is expected to run by a root user or with sudo previledge \e[0m"
+    exit 1
+fi
+
+stat() {
+
+    if [ $? -eq 0 ] ; then
+        echo -e "\e[32m success \e[0m"
+    else 
+        echo -e "\e[31m failure \e[0m"
+        exit 2
+    fi  
+}
+echo -e "*********** \e[35m $COMPONENT Installation has started \e[0m ***********"
+
+echo -n "Configuering the $COMPONENT repo :"
+curl -L https://raw.githubusercontent.com/stans-robot-project/${COMPONENT}/main/${COMPONENT}.repo -o /etc/yum.repos.d/${COMPONENT}.repo
+stat $?
+
+echo -n "Installing $COMPONENT :"
+yum install ${COMPONENT}-6.2.11 -y    &>> $LOGFILE
+stat $?
+

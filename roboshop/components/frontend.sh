ddbd3df3422e7bda6ccdb8ce46211efdfb6d57d8
unknown <vikankshakumari09@gmail.com>
2023-07-04 06:49:22 +0530

    update

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -6,40 +6,40 @@ source components/common.sh
 
 echo -e "*********** \e[35m $COMPONENT Installation has started \e[0m ***********"
 
-echo "Installing Nginx :"
+echo -n "Installing Nginx :"
 yum install nginx -y  &>> $LOGFILE
-stat $? 
-
-echo -n "Downloading the frontend component :"
-curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 stat $?
 
-echo -n "performing cleanup: "
+echo -n "Downloading the ${COMPONENT} component :"
+curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
+stat $?
+
+echo -n "Performing Cleanup: "
 cd /usr/share/nginx/html
-rm -rf *   &>> $LOGFILE
+rm -rf *    &>> $LOGFILE
 stat $?
 
-echo -n "Extracting ${COMPONENT} component :"
-unzip /tmp/${COMPONENT}.zip    &>> $LOGFILE
-mv $COMPONENT-main/* .
-mv static/* .  
+echo -n "Extracting ${COMPOMENT} component :"
+unzip /tmp/${COMPONENT}.zip   &>> $LOGFILE
+mv $COMPONENT-main/*  .
+mv static/* . 
 rm -rf ${COMPONENT}-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
-stat $?
+stat $? 
 
-echo -n "Updating the backend component reveseproxy details : "
-for component in catalogue user cart; do
+echo -n "Updating the Backend component reveseproxy details : "
+for component in catalogue user cart shipping payment; do
     sed -i -e "/$component/s/localhost/$component.roboshop.internal/"  /etc/nginx/default.d/roboshop.conf
 done 
 stat $? 
 
-echo -n "Starting $COMPONENT service : "
-systemctl daemon-reload  &>> $LOGFILE
+echo -n "Starting $COMPONENT service: "
+systemctl daemon-reload &>> $LOGFILE
 systemctl enable nginx  &>> $LOGFILE
 systemctl restart nginx   &>> $LOGFILE
 stat $?
 
-echo -e "************ \e[35m $COMPONENT Installation is completed \e[0m ************"
+echo -e "*********** \e[35m $COMPONENT Installation has Completed \e[0m ***********"
 
 
 # unzip /tmp/frontend.zip

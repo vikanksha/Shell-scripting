b60188d69ad75841bbfa62b61b111be9b4186178
unknown <vikankshakumari09@gmail.com>
2023-07-04 07:46:39 +0530

    update

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -28,7 +28,7 @@ mv localhost.conf /etc/nginx/default.d/roboshop.conf
 stat $? 
 
 echo -n "Updating the Backend component reveseproxy details : "
-for component in catalogue user; do
+for component in catalogue user cart ; do
     sed -i -e "/$component/s/localhost/$component.roboshop.internal/"  /etc/nginx/default.d/roboshop.conf
 done 
 stat $? 

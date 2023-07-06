afb8b94f6d364a3c5db57b3b7786455e08f2775f
unknown <vikankshakumari09@gmail.com>
2023-07-06 13:40:46 +0530

    update

--- roboshop/components/user.sh
+++ roboshop/components/user.sh
@@ -1,64 +1,54 @@
-#!/bin/bash
-COMPONENT=user
+#!/bin/bash 
+COMPONENT="user"
 
 source components/common.sh
 
-echo -e "*********** \e[35m $COMPONENT Installation has started \e[0m ***********"
+echo -e "************ \e[35m $COMPONENT Installation has started \e[0m ************"
 
-echo -e "Configuring the $COMPONENT repo :"
+echo -n "Configuring the $COMPONENT repo :"
 curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -  &>> $LOGFILE
 stat $?
 
-echo -e "Installing NodeJS :"
- yum install nodejs -y   &>> LOGFILE
- stat $?
+echo -n "Installing NodeJS:"
+yum install nodejs -y   &>> $LOGFILE
+stat $?
 
-id $APPUSER &>> LOGFILE
+id $APPUSER &>> $LOGFILE
 if [ $? -ne 0 ] ; then
-  echo -n "Creating the service Account :"
-  useradd $APPUSER &>> $LOGFILE
-  stat $?
+    echo -n "creating the service account :"
+    useradd $APPUSER &>> $LOGFILE
+    stat $?
 fi
 
-echo -n "Downloading the $COMPONENT component :"
-curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
-stat $?
+    echo -n "Downloading the $COMPONENT component :"
+    curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
+    stat $? 
 
-echo -n "Copying the $COMPONENT to $APPUSER home directory :"
-cd /home/${APPUSER}/
-rm -rf  ${COMPONENT}  &>> $LOGFILE
-unzip -o /tmp/${COMPONENT}.zip  &>> $LOGFILE
-stat $?
+    echo -n "Copying the $COMPONENT to $APPUSER home directory :"
+    cd /home/${APPUSER}/
+    rm -rf  ${COMPONENT}  &>> $LOGFILE
+    unzip -o /tmp/${COMPONENT}.zip  &>> $LOGFILE
+    stat $?
 
-echo -n "Modifying the ownership :"
-mv $COMPONENT-main/ $COMPONENT
-chown -R $APPUSER:$APPUSER /home/roboshop/$COMPONENT/
-stat $?
+    echo -n "Modifying the ownership :"
+    mv $COMPONENT-main/ $COMPONENT
+    chown -R $APPUSER:$APPUSER /home/roboshop/$COMPONENT/
+    stat $?
 
-echo -n "Generating npm $COMPONENT artifacts :"
-cd /home/${APPUSER}/${COMPONENT}/
-npm install &>> $LOGFILE
-stat $?
+    echo -n "Generating npm $COMPONENT artifacts :"
+    cd /home/${APPUSER}/${COMPONENT}/
+    npm install  &>> $LOGFILE
+    stat $?
 
-echo -n "Updating the $COMPONENT systemd file :"
-sed -i -e 's/AMQPHOST/rabbitmq.roboshop.internal/' -e 's/USERHOST/user.roboshop.internal/' -e 's/CARTHOST/cart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/${APPUSER}/${COMPONENT}/systemd.service  
-mv /home/${APPUSER}/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
-stat $? 
+    echo -n "Updating the $COMPONENT systemd file :"
+    sed -i -e 's/AMQPHOST/rabbitmq.roboshop.internal/' -e 's/USERHOST/user.roboshop.internal/' -e 's/CARTHOST/cart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/${APPUSER}/${COMPONENT}/systemd.service  
+    mv /home/${APPUSER}/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
+    stat $? 
 
+    echo -n "Starting ${COMPONENT} service :"
+    systemctl daemon-reload   &>> $LOGFILE
+    systemctl enable $COMPONENT  &>> $LOGFILE
+    systemctl restart $COMPONENT &>> $LOGFILE
+    stat $? 
 
-
-echo -n "Starting ${COMPONENT} service :"
-systemctl daemon-reload   &>> $LOGFILE
-systemctl enable $COMPONENT  &>> $LOGFILE
-systemctl restart $COMPONENT &>> $LOGFILE
-stat $? 
-
-echo -e "*********** \e[35m $COMPONENT Installation has Completed \e[0m ***********"
-
-
-# $ curl -s -L -o /tmp/user.zip "https://github.com/stans-robot-project/user/archive/main.zip"
-# $ cd /home/roboshop
-# $ unzip /tmp/user.zip
-# $ mv user-main user
-# $ cd /home/roboshop/user
-# $ npm install
+    echo -e "*********** \e[35m $COMPONENT Installation has Completed \e[0m ***********"

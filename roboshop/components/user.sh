ee39b5a2e3b06c18806d340ae2b0429507a714a4
unknown <vikankshakumari09@gmail.com>
2023-07-06 13:56:38 +0530

    update

--- roboshop/components/user.sh
+++ roboshop/components/user.sh
@@ -20,35 +20,35 @@ if [ $? -ne 0 ] ; then
     stat $?
 fi
 
-    echo -n "Downloading the $COMPONENT component :"
-    curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
-    stat $? 
+echo -n "Downloading the $COMPONENT component :"
+curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
+stat $? 
 
-    echo -n "Copying the $COMPONENT to $APPUSER home directory :"
-    cd /home/${APPUSER}/
-    rm -rf  ${COMPONENT}  &>> $LOGFILE
-    unzip -o /tmp/${COMPONENT}.zip  &>> $LOGFILE
-    stat $?
+echo -n "Copying the $COMPONENT to $APPUSER home directory :"
+cd /home/${APPUSER}/
+rm -rf  ${COMPONENT}  &>> $LOGFILE
+unzip -o /tmp/${COMPONENT}.zip  &>> $LOGFILE
+stat $?
 
-    echo -n "Modifying the ownership :"
-    mv $COMPONENT-main/ $COMPONENT
-    chown -R $APPUSER:$APPUSER /home/roboshop/$COMPONENT/
-    stat $?
+echo -n "Modifying the ownership :"
+mv $COMPONENT-main/ $COMPONENT
+chown -R $APPUSER:$APPUSER /home/roboshop/$COMPONENT/
+stat $?
 
-    echo -n "Generating npm $COMPONENT artifacts :"
-    cd /home/${APPUSER}/${COMPONENT}/
-    npm install  &>> $LOGFILE
-    stat $?
+echo -n "Generating npm $COMPONENT artifacts :"
+cd /home/${APPUSER}/${COMPONENT}/
+npm install  &>> $LOGFILE
+stat $?
 
-    echo -n "Updating the $COMPONENT systemd file :"
-    sed -i -e 's/AMQPHOST/rabbitmq.roboshop.internal/' -e 's/USERHOST/user.roboshop.internal/' -e 's/CARTHOST/cart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/${APPUSER}/${COMPONENT}/systemd.service  
-    mv /home/${APPUSER}/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
-    stat $? 
+echo -n "Updating the $COMPONENT systemd file :"
+sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/${APPUSER}/${COMPONENT}/systemd.service  
+mv /home/${APPUSER}/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
+stat $? 
 
-    echo -n "Starting ${COMPONENT} service :"
-    systemctl daemon-reload   &>> $LOGFILE
-    systemctl enable $COMPONENT  &>> $LOGFILE
-    systemctl restart $COMPONENT &>> $LOGFILE
-    stat $? 
+echo -n "Starting ${COMPONENT} service :"
+systemctl daemon-reload   &>> $LOGFILE
+systemctl enable $COMPONENT  &>> $LOGFILE
+systemctl restart $COMPONENT &>> $LOGFILE
+stat $? 
 
-    echo -e "*********** \e[35m $COMPONENT Installation has Completed \e[0m ***********"
+echo -e "*********** \e[35m $COMPONENT Installation has Completed \e[0m ***********"

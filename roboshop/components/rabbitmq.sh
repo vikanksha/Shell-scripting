a131b0deccdcf9ba25c757bfb05562e1951f2d36
unknown <vikankshakumari09@gmail.com>
2023-07-07 07:32:58 +0530

    new componnet

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-echo "I am cart"
+COMPONENT="rabbitmq"
+
+source components/common.sh
+
+echo -e "*********** \e[35m $COMPONENT Installation has started \e[0m ***********"
+
+echo -n "Configuring the $COMPONENT repo :"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash  &>> LOGFILE
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>> LOGFILE
+stat $?
+
+echo -n "Installing $COMPONENT :"
+yum install rabbitmq-server -y  &>> LOGFILE
+stat $?
+
